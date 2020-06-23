a72448e1769522cc51f9478fb5d73d030df82e16
Brian Sunday <brians931@gmail.com>
2020-06-23 16:09:05 +0300

    add create logs handler

---
+++ backend/src/handlers/create-logs.js
@@ -0,0 +1,27 @@
+module.exports = (db) => {
+  return async (req, res) => {
+    const {
+      programTime,
+      event,
+      message,
+      actualTime,
+      displayMessage,
+    } = req.body;
+
+    const data = {
+      programTime,
+      event,
+      message,
+      actualTime,
+      displayMessage,
+    };
+
+    try {
+      const result = await db.collection("logs").insertOne(data);
+      res.status(200).json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(200).json(result);
+    }
+  };
+};
