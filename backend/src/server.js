5a352449125e423e1576885bf6a8a4b861188907
Brian Sunday <brians931@gmail.com>
2020-06-24 22:42:58 +0300

    add logic to initialize clock at 1200

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,6 +22,7 @@ const start = async () => {
   const io = socketIo(server);
 
   let interval;
+  let totalSeconds = 43200; // rep 12hrs
 
   io.on("connection", (socket) => {
     console.log("===================");
@@ -40,7 +41,19 @@ const start = async () => {
   });
 
   const getApiAndEmit = (socket) => {
-    const response = new Date();
+    ++totalSeconds;
+    var hour = Math.floor(totalSeconds / 3600); //12
+    var minute = Math.floor((totalSeconds - hour * 3600) / 60); // 43380 - 43300 = 180/60 = 3mins
+    var seconds = totalSeconds - (hour * 3600 + minute * 60);
+    if (hour < 10) hour = "0" + hour;
+    if (minute < 10) minute = "0" + minute;
+    if (seconds < 10) seconds = "0" + seconds;
+
+    let response = {
+      hour,
+      minute,
+      seconds,
+    };
     // emit a new message which will be consumed by the client
     socket.emit("dateFromApi", response);
   };
