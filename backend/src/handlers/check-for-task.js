163bbea0e256dab1d1ae203fff2fd102247ccf6f
Brian Sunday <brians931@gmail.com>
2020-06-26 02:12:45 +0300

    stop task working

--- backend/src/handlers/check-for-task.js
+++ backend/src/handlers/check-for-task.js
@@ -1,4 +1,9 @@
-const { updateSupervisor, createLogs } = require("../utils/mongo");
+const {
+  updateSupervisor,
+  createLogs,
+  updateSupervisorStop,
+  createLogsStop,
+} = require("../utils/mongo");
 
 module.exports = (db) => {
   // return a valid express handler
@@ -10,6 +15,19 @@ module.exports = (db) => {
       try {
         const supervisor = await updateSupervisor(db); // update supervisor
         const logger = await createLogs(db); // create logs
+
+        res.json(logger.ops);
+      } catch (error) {
+        res.status(500).json(error);
+        console.error(error);
+      }
+    }
+
+    if (programTime == global.stopIsHappening) {
+      // means stop event is taking place
+      try {
+        const supervisor = await updateSupervisorStop(db); // update supervisor
+        const logger = await createLogsStop(db); // create logs
 
         res.json(logger.ops);
       } catch (error) {
